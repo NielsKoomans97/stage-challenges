71540c2a2172b3b2d99949c51c208686a53c67ba
Niels Koomans <niels.koomans@gmail.com>
2023-02-01 10:58:32 +0100

    added json path finder for challenge 6

---
+++ 6/challenges/jsonpathFinder2/Program.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Diagnostics;
+using System.IO;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var stp = new Stopwatch();
+
+        stp.Start();
+
+        string path = $"{AppDomain.CurrentDomain.BaseDirectory}\\challenge2.txt";
+
+        string file = File.ReadAllText(path);
+        JObject? obj = (JObject?)JsonConvert.DeserializeObject(file);
+
+        var values = obj?.Descendants()
+            .Where(a => a.Type == JTokenType.String
+             && (string?)a == "dailyprogrammer")
+            ?? throw new NullReferenceException();
+
+        foreach (var value in values)
+        {
+            Console.WriteLine($"{value.Path}");
+        }
+
+        stp.Stop();
+        Console.WriteLine(stp.Elapsed);
+
+        Console.ReadKey();
+    }
+}
--- 6/challenges/processor.rb
+++
@@ -1,7 +0,0 @@
-require 'rubygems'
-require 'json'
-require 'pp'
-
-json = File.read('C:\Users\Niels.niels-pc\source\repos\stage-challenges\6\challenges\challenge1.txt')
-obj = JSON.parse(json)
-
